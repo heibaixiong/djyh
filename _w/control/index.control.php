3b18b0974d234537817a398d70208d82ec25dc9e
chaifeifei <chaishufei@163.com>
2016-05-19 17:19:41 +0800

    edit register and login of weixin

--- _w/control/index.control.php
+++ _w/control/index.control.php
@@ -53,55 +53,54 @@ function __ads(){
 	}
 }
 
-//微信端自动登录注册
+//weixin login and register
 function __auth(){
-	global $_wrap;
-	require_once(APP_PATH . 'library/wxpayexception.php');
-	define('APPID', $_wrap['wxpay_config']['wxpay_appid']);
-	define('MCHID', $_wrap['wxpay_config']['wxpay_mchid']);
-	define('KEY', $_wrap['wxpay_config']['wxpay_key']);
-	define('APPSECRET', $_wrap['wxpay_config']['wxpay_appsecret']);
-	define('SSLCERT_PATH', $_wrap['wxpay_config']['sslcert_path']);
-	define('SSLKEY_PATH', $_wrap['wxpay_config']['sslkey_path']);
-	define('CURL_PROXY_HOST', '0.0.0.0');
-	define('CURL_PROXY_PORT', 0);
-	define('REPORT_LEVENL', 1);
-
-	require_once(APP_PATH . 'library/wxpaydata.php');
-	require_once(APP_PATH . 'library/wxpayapi.php');
-	require_once(APP_PATH . 'library/wxpayconfig.php');
-	require_once(APP_PATH . 'library/wxpayjsapipay.php');
-
-	$tools = new JsApiPay();
-	$openId = $tools->GetOpenid();
-	//var_dump($openId);exit;
-	_session('weixin_openid', $openId);
-
+	//_session('weixin_openid', 'oivF8wVNcYmaPoOTbX11lgA1oqCo');
+	$openId = _session('weixin_openid');
+	if(!$openId || $openId == ''){
+		exit;
+	}
 	$wx_user = _sqlone('admin_bind', 'openid=\'' . _escape($openId) . '\'');
+	//var_dump($wx_user);exit;
 	if (empty($wx_user)) {
 		$data1 = array(
 				'openid' => $openId,
 				'from' => 'weixin',
 				'add_time' => time()
 		);
-		$wx_id = _sqlinsert('admin_bind', $data1);  //插入绑定表
+		//var_dump($data1);exit;
+		$wx_id = _sqlinsert('admin_bind', $data1);  //insert bindinfo to admin_bind
 
 		$data2 = array(
 				'user' => 'w' . _random(8, 'num'),
 				'rank' => '5',
-				'reg_time' => time(),
+				'regtime' => time(),
 				'updatetime' => time(),
 				'login' => '1',
 				'state' => '0'
 		);
-		$user_id = _sqlinsert('admin', $data2); //插入用户表
-		_sqldo('update ' . _pre('admin_bind') . ' set user_id=' . $user_id . ' where id=' . $wx_id);  //更新绑定表
-
-		_session('webid', $user_id);
+		if($user_id = _sqlinsert('admin', $data2)) {  //insert userinfo to admin_bind
+			_sqldo('update ' . _pre('admin_bind') . ' set user_id=' . $user_id . ' where id=' . $wx_id);  //update userinfo to admin_bind
+			_session('webid', $user_id);
+		}
 	} else {
-		_session('webid', $wx_user['user_id']);
+		//get table admin info
+		$user = _sqlone('admin', 'id = '.$wx_user['user_id']);
+		if(empty($user)){	//add user info to table of admin
+			$data['user'] = 'w' . _random(8, 'num');
+			$data['rank'] = '5';
+			$data['regtime'] = time();
+			$data['updatetime'] = time();
+			$data['login'] = 1;
+			$data['state'] = 0;
+			if($user_id = _sqlinsert('admin', $data)) {  //insert userinfo to admin_bind
+				_sqldo('update ' . _pre('admin_bind') . ' set user_id=' . $user_id . ' where id=' . $wx_user['id']);  //update userinfo to admin_bind
+			}
+		}else{
+			_sqldo('update ' . _pre('admin') . ' set updatetime=' . time() . ', login = login + 1 where id=' . $user['id']);	//update userinfo(updatetime,login) to admin
+			_session('webid', $wx_user['user_id']);
+		}
 	}
-
 	if (_session('weixin_redirect_url')) {
 		_header(_session('weixin_redirect_url'));
 	} else {
