43b2e83e41b36998b7948b72f5b3ebcfaf77ad4a
chaifeifei <chaishufei@163.com>
2016-05-20 09:50:26 +0800

    edit pay style

--- _w/tpl/cart/submit.php
+++ _w/tpl/cart/submit.php
@@ -14,21 +14,21 @@
 <!-- 主体 -->
 <div class="main-Body">
     <div class="main-content">
-        <div class="main-body-w">
-            <h4 class="hBorder">订单提交成功</span>
-                <div class="progress-jhd3"></div>
-            </h4>
+        <div class="main-body-w" style="margin:0 10px;">
+            <div class="hBorder" style="color:#ff0000;text-align: center;line-height:100px;font-size:16px;">订单提交成功</div>
             <div id="orderTotalInfo" class="gross-price-js">
                 <div class="fl gross-left">
                 您的订单已成功提交！我们会尽快为您安排处理！
                 </div>
                 <?php if (isset($_['payment_data'])) { ?>
-                <div class="fr gross-right">
+                <div class="fr gross-right" style="margin-top:20px;">
                     <div class="fl">
-                        <p class="fl">总金额：<b class="red font18"><strong>￥<?php echo _rmb($_['order']['total']); ?></strong></b></p>
+                        <p class="fl" style="line-height:30px;text-align:center;">总金额：<b class="red font18"><strong>￥90<?php echo _rmb($_['order']['total']); ?></strong></b></p>
                     </div>
-                    <?php echo $_['payment_data']; ?>
-                    <p class="fl"><a class="return-btn" href="<?php echo _u('/cart/checkout/');?>" id="btn-cart-wait">稍后支付</a></p>
+                    <div style="background-color: #ff5656;height:30px;color:#fff;text-align: center;border-radius:3px;line-height:30px;">
+                        <?php echo $_['payment_data']; ?>
+                    </div>
+                    <p class="fl" style="margin-top:30px;text-align: right;"><a class="return-btn" href="<?php echo _u('/person/order/');?>" id="btn-cart-wait">稍后支付</a></p>
                 </div>
                 <?php } ?>
             </div>
