58a3643c48b4f89f7dbf4d215f44f2776b4acb31
chaifeifei <chaishufei@163.com>
2016-05-20 17:17:45 +0800

    add good attr

--- _w/tpl/shop/show.php
+++ _w/tpl/shop/show.php
@@ -4,7 +4,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="maximum-scale=1.0,minimum-scale=1.0,user-scalable=0,width=device-width,initial-scale=1.0"/>
 	<meta name="format-detection" content="telephone=no,email=no,date=no,address=no">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?php echo $_['title'];?></title>
 	<?php
 	_css('aui');
@@ -16,7 +15,7 @@
 <body>
 <!--头部-->
 <header class="aui-nav aui-bar aui-bar-nav aui-bar-dark" id="top_nav">
-	<div class="aui-col-xs-2" style="width:15%;" onclick="history.go(-1)">
+	<div class="aui-col-xs-2" style="width:18%;">
         <span class="aui-pull-left" style="padding-left: 5px;">
            <span class="aui-iconfont aui-icon-left"></span>
         </span>
@@ -29,22 +28,20 @@
 			</form>
 		</div>
 	</div>
-	<div class="aui-col-xs-2" style="width:23%;">
+	<div class="aui-col-xs-2" style="width:20%;">
          <span class="aui-pull-right" style="padding-right:5px;padding-top:2px;">
-            <a class="index_car good-search">搜索</a>
+            <a class="index_car">搜索</a>
         </span>
 	</div>
 </header>
-<div style="position: absolute;top: 50px;bottom: 55px;overflow-y: scroll;-webkit-overflow-scrolling: touch; width:100%; ">
+<div  style="position: absolute;top: 50px;bottom: 55px;overflow-y: scroll;-webkit-overflow-scrolling: touch; width:100%; ">
 
 	<!--筛选条件-->
-	<ul class="aui-content list_sx aui-border-tb">
-		<li class="aui-col-xs-6 current_mr"><a href="#">商品</a></li>
-		<li class="aui-col-xs-6 "><a href="#">详情</a></li>
-		<!--
+	<ul class="aui-content list_sx aui-border-tb" style="margin-bottom:0;">
+		<li class="aui-col-xs-3 current_mr"><a href="#">商品</a></li>
+		<li class="aui-col-xs-3 "><a href="#">详情</a></li>
 		<li class="aui-col-xs-3 "><a href="#">评价</a></li>
-		<li class="aui-col-xs-3 "><a href="#">购物车</a></li>
-		-->
+		<li class="aui-col-xs-3 " style="border: none"><a href="#">购物车</a></li>
 	</ul>
 
 	<!--main-->
@@ -52,7 +49,7 @@
 		<!-- tab01-->
 		<div class="det_tab01" >
 			<div class="det_img">
-				<img src="<?php echo _resize($_['rs']['img'], 360, 360); ?>" />
+				<img src="<?php echo _resize($_['rs']['img'], 360, 360); ?>">
 			</div>
 			<!--题目-->
 			<div class="det_tit">
@@ -77,7 +74,7 @@
 				</div>
 			</div>
 			<!--选择-->
-			<ul class="aui-list-view det_bigbox" style="margin:10px 0;border-color:#c6c6c6;display: none;">
+			<ul class="aui-list-view det_bigbox" style="margin:10px 0;border-color:#c6c6c6;">
 				<li class="aui-list-view-cell">
 					<div class="aui-arrow-right aui-ellipsis-1 list_pp">
 						选择套装
@@ -135,16 +132,7 @@
 			<div class="d_tab_main">
 				<div class="d_tab_list">
 					<p class="d_tab_list_title">商品详情</p>
-					<div id="content">
-						<style>
-							#content img{max-width: 100%;}
-						</style>
-						<?php echo $_['rs']['content']; ?>
-					</div>
-				</div>
-				<!--
-				<div class="d_tab_list">
-					<p class="d_tab_list_title">商品详情</p>
+					<!--
 					<ul class="d_tab_list02">
 						<li><p>产品参数</p><li>
 						<li>生产许可证编号：QS4419 0601 1036</li>
@@ -153,7 +141,9 @@
 						<li>厂址：西华县逍遥镇工业区</li>
 						<li>厂家联系方式：03942581180</li>
 					</ul>
+					-->
 					<ul class="d_tab_list02">
+						<!--
 						<li><p>规格参数</p><li>
 						<li><a class="d_tab_list02_lef">品牌: 逍遥老杨家</a><a class="d_tab_list02_rig">特产品类: 逍遥镇胡辣汤</a></li>
 						<li><a class="d_tab_list02_lef">保质期：365 天</a><a class="d_tab_list02_rig">包装种类: 袋装</a></li>
@@ -163,17 +153,29 @@
 						<li>规格: 麻组合辣和微辣组合256g*2袋</li>
 						<li><a class="d_tab_list02_lef">产地: 中国大陆</a><a class="d_tab_list02_rig">省份: 河南省</a></li>
 						<li><a class="d_tab_list02_lef">城市: 郑州市</a><a class="d_tab_list02_rig">口味: 胡辣味</a></li>
+						-->
+						<?php
+						if (!empty($_['para_info'])) {
+						?>
+
+						<li><p>规格参数</p><li>
+						<?php foreach ($_['para_info'] as $para) { ?>
+						<li><?php echo $para['paraname'].'：'.$para['value']; ?></li>
+						<?php
+							}
+						}
+						?>
 					</ul>
 				</div>
 				<div class="d_tab_img">
 					<p class="d_tab_list_title">图文详情</p>
-					<img src="images/dta_pro01.png">
-					<img src="images/dta_pro02.png">
-					<img src="images/dta_pro03.png">
-					<img src="images/dta_pro04.png">
-					<img src="images/dta_pro05.png">
+					<div id="content">
+						<style>
+							#content img{max-width: 100%;}
+						</style>
+						<?php echo $_['rs']['content']; ?>
+					</div>
 				</div>
-				-->
 			</div>
 		</div>
 		<!--热门推荐-->
@@ -182,19 +184,21 @@
 			<ul class="detail_hot_min">
 				<?php
 				foreach($_['list'] as $k => $v){
-					?>
-					<li>
-						<div class="detail_hot_img">
-							<a href="<?php echo _u('/shop/show/'.$v['id']); ?>"><img src="<?php echo _resize($v['img'], 210, 210); ?>"></a>
-							<?php if ($v['recommend'] == 1) { ?>
+				?>
+				<li>
+					<div class="detail_hot_img">
+						<a href="<?php echo _u('/shop/show/'.$v['id']); ?>">
+							<img src="<?php echo _resize($v['img'], 210, 210); ?>">
+						</a>
+						<?php if ($v['recommend'] == 1) { ?>
 							<span class="store_one">推荐<br>商品</span>
-							<?php } elseif ($v['hot'] == 1) { ?>
-								<span class="store_one">热门<br>商品</span>
-							<?php } ?>
-						</div>
-						<p class="detail_hot_wz"><a href="<?php echo _u('/shop/show/'.$v['id']); ?>" class="aui-ellipsis-1"><?php echo $v['title']; ?></a></p>
-						<p class="detail_hot_wz02"><span class="detail_hot_pice">￥<?php echo _rmb($_['rs']['mark']/100);?></span><span class="detail_hot_num">已售<em><?php echo $_['rs']['sale'];?></em>件</span></p>
-					</li>
+						<?php } elseif ($v['hot'] == 1) { ?>
+							<span class="store_one">热门<br>商品</span>
+						<?php } ?>
+					</div>
+					<p class="detail_hot_wz"><a href="<?php echo _u('/shop/show/'.$v['id']); ?>" class="aui-ellipsis-1"><?php echo $v['title']; ?></a></p>
+					<p class="detail_hot_wz02"><span class="detail_hot_pice">￥<?php echo _rmb($_['rs']['mark']/100);?></span><span class="detail_hot_num">已售<em><?php echo $_['rs']['sale'];?></em>件</span></p>
+				</li>
 					<?php
 				}
 				?>
@@ -208,97 +212,85 @@
 				<a href="#" class="attr_bottom_a03">立即购买</a>
 			</div>
 		</div>
-
 	</div>
-
-
 </div>
 
+<?php
+_part('footer');
+_js('iscroll');
+?>
 
 <!--弹出层-->
 <div class="slide-mask"></div>
-<aside class="slide-wrapper" >
-	<div class="slide_big">
-		<div class="slide_right">
-			<div class="detail_attr" >
-				<ul>
-					<li class="aui-content index_content det_newcont" style="background: #fff;padding-top:20px;">
-						<div class="aui-col-xs-4" >
-							<a href="#" class="index_pro02">
-								<img src="<?php echo _resize($_['rs']['img'], 300, 300); ?>" >
-							</a>
-						</div>
-						<div class="aui-col-xs-8" >
-							<div class="index_bleft">
-								<p class="index_text01"><a href="#"><?php echo $_['rs']['title']; ?></a></p>
-								<p class="index_text02"><?php echo $_['rs']['class1name']; ?></p>
-								<p class="index_text03">￥<?php echo _rmb($_['rs']['mark']/100);?></p>
-							</div>
-						</div>
-					</li>
-					<li>
-						<div class="attr_middle">
-							<dl>
-								<dt>选择套装</dt>
-								<dd><a>精品礼盒</a><a>实惠套装</a><a>简易装</a><a>简易装</a><a>简易装</a><a>简易装</a></dd>
-							</dl>
-						</div>
-					</li>
-					<li>
-						<div class="attr_center">
-							<dl>
-								<dt>数量</dt>
-								<dd class="detail_sl">
-									<span class="detail_jian"></span>
-									<input type="text" value="1">
-									<span class="detail_jia"></span>
-								</dd>
-							</dl>
-						</div>
-						<div style="clear: both;"></div>
-					</li>
-					<li>
-						<div class="attr_bottom">
-							<a href="#" class="attr_bottom_a01">进  店</a>
-							<a href="#" class="attr_bottom_a02 idnex_gw" data-id="<?php echo $_rs['id']; ?>">加入购物车</a>
-							<a href="#" class="attr_bottom_a03">立即购买</a>
-						</div>
-					</li>
-				</ul>
+
+<div class="detail_attr" >
+	<ul>
+		<li class="aui-content index_content det_newcont" style="background: #fff;padding-top:20px;position:absolute;top:0;left:0;width:100%;height:120px;">
+			<div class="aui-col-xs-3" >
+				<a href="#" class="index_pro02">
+					<img src="<?php echo _resize($_['rs']['img'], 360, 360); ?>" />
+				</a>
+
 			</div>
+			<div class="aui-col-xs-9" >
+				<div class="index_bleft">
+					<p class="index_text01"><a href="#"><?php echo $_['rs']['title']; ?></a></p>
+					<p class="index_text02"><?php echo _left(strip_tags($_['rs']['content']), 0, 48, '...'); ?></p>
+					<p class="index_text03">￥<?php echo _rmb($_['rs']['mark']/100);?></p>
+				</div>
+			</div>
+		</li>
 
+
+		<div class="attr_main" id="wrapper" >
+			<ul>
+				<?php if(!empty($_['attr_info'])){ ?>
+				<li>
+					<div class="attr_middle">
+						<dl>
+							<dt>选择套装</dt>
+							<dd>
+								<?php
+								foreach($_['attr_info'] as $k => $v){
+									echo '<a>' . $v['model'] . '</a>';
+								}
+								?>
+								<!-- <a>精品礼盒</a> -->
+							</dd>
+						</dl>
+					</div>
+				</li>
+				<?php } ?>
+				<li>
+					<div class="attr_center">
+						<dl>
+							<dt>数量</dt>
+							<dd class="detail_sl">
+								<span class="detail_jian"></span>
+								<input type="text" value="1">
+								<span class="detail_jia"></span>
+							</dd>
+						</dl>
+					</div>
+					<div style="clear: both;"></div>
+				</li>
+			</ul>
 		</div>
+		<li class="attr_bombox">
+			<div class="attr_bottom" style="width:100%;">
+				<a href="#" class="attr_bottom_a01">进  店</a>
+				<a href="#" class="attr_bottom_a02">加入购物车</a>
+				<a href="#" class="attr_bottom_a03">立即购买</a>
+			</div>
+		</li>
+	</ul>
+	<span class="detail_close"></span>
+</div>
 
-	</div>
-
-
-</aside>
-<?php
-_part('footer');
-?>
 <script>
-
-
-	var arr_old_bgp = ["footer_icon01","footer_icon02","footer_icon03","footer_icon04"],
-			arr_new_bgp = ["footer_bh01","footer_bh02","footer_bh03",'footer_bh04',];
-	$('.aui-bar-tab li').click(function(){
-		var guide =$(this).index();
-		var $this = $(this),
-				index = $this.index();
-		$this.parent().find('li').each(function(i,e){
-
-			$(this).find('span').removeClass(arr_new_bgp[i]);
-
-		});
-
-		$(this).find('span').addClass(arr_new_bgp[index]).siblings("p").css("color","#ff0000").parents("li").siblings("li").find("span").removeClass(arr_new_bgp[index]).siblings("p").css("color","#3c3c3c");
-	});
-
-
 	var arr_old_bg= ["","list_sxicon01","list_sxicon02","list_sxicon03"],
 			arr_new_bg = [" ", "current_mr02","current_mr03","current_mr04"];
 	$('.list_sx li').click(function(){
-
 		var guide =$(this).index();
 		var $this = $(this),
 				index = $this.index();
@@ -313,22 +305,6 @@ _part('footer');
 		$(".det_tab01").eq(index).show().siblings(".det_tab01").hide();
 	});
 
-
-	$('div.slide-mask').on('click', function(){
-		$('div.slide-mask').hide();
-		$('aside.slide-wrapper').removeClass('moved');
-//         $(".slide-wrapper").hide();
-
-	});
-	$(".det_bigbox li").click(function(){
-		var wh = $('div.wrapperhove'+'rtree').height();
-		$('div.slide-mask').css('height', wh).show();
-		$('aside.slide-wrapper').css('height', wh).addClass('moved');
-		$('div.slide-mask').css({'bottom':'0'});
-		$('.slide-wrapper ').css({'bottom':'0'});
-		$(".list_bigbox02").show();
-	})
-
 	//购物车点击操作
 	$(".idnex_gw").click(function(){
 		var tag = is_login();
@@ -339,9 +315,32 @@ _part('footer');
 		}
 	});
 
+
+	$('div.slide-mask').on('click', function(){
+		$('div.slide-mask').hide();
+		$('aside.slide-wrapper').removeClass('moved');
+		$('.detail_attr').removeClass('moved');
+		$('body').unbind('touchmove');
+	});
+	$(".det_bigbox li").click(function(){
+		var wh = $('div.wrapperhove'+'rtree').height();
+		$('div.slide-mask').css('height', wh).show();
+		$('.detail_attr').css('height', wh).addClass('moved');
+		$('div.slide-mask').css({'bottom':'0'});
+		$('.detail_attr ').css({'bottom':'0'});
+		$('body').on('touchmove',function(e){
+			e.preventDefault();
+		})
+		var myScroll;
+		myScroll = new IScroll('#wrapper', { click: true, fadeScrollbars: true,zoom:true});
+	})
+
+	$('.detail_close').on('click', function(){
+		$('div.slide-mask').hide();
+		$('.detail_attr').removeClass('moved');
+		$('body').unbind('touchmove');
+
+	});
 </script>
-
-
-
 </body>
 </html>
